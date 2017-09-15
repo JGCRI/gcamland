f936bdac58c39c7d7d61f621cc53f74bf7a4e947
D3X132 <katherine.calvin@pnnl.gov>
2017-09-15 16:51:03 -0400

    Removing unneeded constants

--- R/constants.R
+++ R/constants.R
@@ -1,9 +1,10 @@
 # constants.R
 
-DEBUG <- FALSE
+# Time-related parameters
 FINAL_CALIBRATION_PERIOD <- 2010
 YEARS <- c(2010, 2020)
 
+# Threshold-related constants
 # TODO: Make sure these are consistent with GCAM
 LARGE_NUMBER <- 1e9
 SMALL_NUMBER <- 1e-6
@@ -14,12 +15,3 @@ UNMANAGED_LAND_VALUE <- 0.5
 LOGIT_EXPONENT <- 0.75
 REGION <- "USA"
 LAND_ALLOCATION <- 200
-
-tibble::tibble(name = c("Crop1", "Crop2"),
-               price = c(1, 2),
-               cost = c(0, 1),
-               yield = c(1, 1),
-               area = c(100, 100)) -> LANDLEAF_CALDATA
-
-tibble::tibble(name = c("Land"),
-               area = c(200)) -> LANDNODE_CALDATA
