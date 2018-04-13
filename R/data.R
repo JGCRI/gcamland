04e32e77233cd6cfa2681259a5f3b39a27bf91c0
Robert Link <robert.link@pnnl.gov>
2018-04-13 09:51:18 -0400

    Add FAO historical land use data set.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Historical land use data
+#'
+#' This dataset contains land use by GCAM region and GCAM commodity for the
+#' years 1961--2010
+#'
+#' @format A data frame with 14953 rows and 4 variables:
+#' \describe{
+#' \item{region}{GCAM region name.}
+#' \item{GCAM_commodity}{GCAM commodity name.}
+#' \item{year}{Year of the observation.}
+#' \item{area}{Land area allocated to that commodity for the region and year.}
+#' }
+#' @source Kate!
+"FAO_land_history"
