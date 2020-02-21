7c888a364fc48e0180ea5845cc6b6b6863c1eaa0
kvcalvin <katherine.calvin@pnnl.gov>
2020-02-21 16:37:01 -0500

    Update historical cost data
    Use USDA cost information in the historical period

--- R/read_data.R
+++ R/read_data.R
@@ -652,6 +652,8 @@ ReadData_AgProd <- function(aRegionName, ascentype, aSubRegion, subregionData=NU
       bioYield
     suppressMessages(read_csv(system.file("extdata", "./initialization-data/L201.AgHAtoCL.csv", package = "gcamland"), skip = 3)) ->
       HAtoCL
+    suppressMessages(read_csv(system.file("extdata", "./initialization-data/AgCostTechChange.csv", package = "gcamland"), skip = 3)) ->
+      costTechChange
 
     # Filter data for the specified region -- note using dplyr::filter for data frames with more than 15000 entries
     calOutput <- filter(calOutput, region == aRegionName)
@@ -659,6 +661,7 @@ ReadData_AgProd <- function(aRegionName, ascentype, aSubRegion, subregionData=NU
     cost <- filter(cost, region == aRegionName)
     bioYield <- subset(bioYield, region == aRegionName)
     HAtoCL <- subset(HAtoCL, region == aRegionName)
+    costTechChange <- subset(costTechChange, region == aRegionName)
 
     # Filter data for specified AEZ
     if(!is.null(AEZ)){
@@ -671,6 +674,7 @@ ReadData_AgProd <- function(aRegionName, ascentype, aSubRegion, subregionData=NU
       cost <- filter(cost, grepl(AEZ, AgSupplySubsector))
       bioYield <- subset(bioYield, grepl(AEZ, AgSupplySubsector))
       HAtoCL <- subset(HAtoCL, grepl(AEZ, AgSupplySubsector))
+      costTechChange <- subset(costTechChange, grepl(AEZ, AgSupplySubsector))
 
     }
 
@@ -680,7 +684,7 @@ ReadData_AgProd <- function(aRegionName, ascentype, aSubRegion, subregionData=NU
       unique() ->
       productName
   }
-  return(structure(list(calOutput, agProdChange, cost, bioYield, HAtoCL, productName),
+  return(structure(list(calOutput, agProdChange, cost, bioYield, HAtoCL, productName, costTechChange),
          rgn = aRegionName, scentype = ascentype))
 }
 
