339c63e41adf84efc441c778baa54c5288fac45a
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2020-05-21 12:31:35 -0400

    add script to hold objective functions and re-build documentation

---
+++ R/objectivefun.R
@@ -0,0 +1,4 @@
+# objectivefun.R
+# Objective functions for use in model evaluation across parameter sets.
+# Can be used for either a brute force approach, or as objective functions in
+# iterative methods approaches.
