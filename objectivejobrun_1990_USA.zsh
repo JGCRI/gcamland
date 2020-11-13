80ebb680d7d5e4950a21d86e533e60dba1912a79
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2020-11-13 08:45:36 -0500

    explicitly write out region

--- objectivejobrun_1990_USA.zsh
+++ objectivejobrun_1990_USA.zsh
@@ -31,9 +31,9 @@ mkdir -p $outdir
 mkdir -p $logdir
 
 echo "Run command:"
-echo "source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', $skip, logdir='$logdir', aDifferentiateParamByCrop = $PARAM, aIncludeSubsidies=$SUBS, aTotalSamplesPlanned = $TOTAL_SAMPLES, aRegion = '$REGION')"
+echo "source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', $skip, logdir='$logdir', aDifferentiateParamByCrop = $PARAM, aIncludeSubsidies=$SUBS, aTotalSamplesPlanned = $TOTAL_SAMPLES, aRegion = "USA")"
 
-Rscript -e "source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', $skip, logdir='$logdir', aDifferentiateParamByCrop = $PARAM, aIncludeSubsidies=$SUBS, aTotalSamplesPlanned = $TOTAL_SAMPLES, aRegion = '$REGION')"
+Rscript -e "source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', $skip, logdir='$logdir', aDifferentiateParamByCrop = $PARAM, aIncludeSubsidies=$SUBS, aTotalSamplesPlanned = $TOTAL_SAMPLES, aRegion = "USA")"
 ## Use this version instead to write log files:
 ## Rscript -e "source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', '$skip', '$logdir')"
 
