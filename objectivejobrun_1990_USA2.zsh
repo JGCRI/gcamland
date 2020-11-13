3d91b4ef9211cbea3cc2ec8f2cda23404a5977ae
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2020-11-13 11:01:41 -0500

    try in a diff place

--- objectivejobrun_1990_USA2.zsh
+++ objectivejobrun_1990_USA2.zsh
@@ -30,12 +30,11 @@ logdir="/pic/projects/GCAM/Abigail/gcamland_output/$REGION/log"
 mkdir -p $outdir
 mkdir -p $logdir
 
-Rscript -e "devtools::load_all('/pic/projects/GCAM/Abigail/gcamland/')"
 
 echo "Run command:"
-echo "source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', $skip, logdir='$logdir', aDifferentiateParamByCrop = $PARAM, aIncludeSubsidies=$SUBS, aTotalSamplesPlanned = $TOTAL_SAMPLES)"
+echo "devtools::load_all('/pic/projects/GCAM/Abigail/gcamland/'); source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', $skip, logdir='$logdir', aDifferentiateParamByCrop = $PARAM, aIncludeSubsidies=$SUBS, aTotalSamplesPlanned = $TOTAL_SAMPLES)"
 
-Rscript -e "source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', $skip, logdir='$logdir', aDifferentiateParamByCrop = $PARAM, aIncludeSubsidies=$SUBS, aTotalSamplesPlanned = $TOTAL_SAMPLES)"
+Rscript -e "devtools::load_all('/pic/projects/GCAM/Abigail/gcamland/'); source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', $skip, logdir='$logdir', aDifferentiateParamByCrop = $PARAM, aIncludeSubsidies=$SUBS, aTotalSamplesPlanned = $TOTAL_SAMPLES)"
 ## Use this version instead to write log files:
 ## Rscript -e "source('$program'); run_ens_obj_analysis('$nodefile', $SLURM_NTASKS, $N, '$outdir', '$skip', '$logdir')"
 
